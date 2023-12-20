abd47d69c873c39d73ec60d3a0b1ee173681b9eb
Md Masum Billah <masumbillah6584@gmail.com>
2023-12-20 15:24:53 +0600

    added socket io

--- src/app/image.tsx
+++ src/app/image.tsx
@@ -1,29 +1,35 @@
-import React ,{useState,useEffect}from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+import { io } from "socket.io-client";
 
 export default function ImageComp() {
-    const [imageUrl, setImageUrl] = useState(
-      "https://images.unsplash.com/photo-1500964757637-c85e8a162699?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHNjZW5lcnl8ZW58MHx8MHx8fDA="
-    );
+  const [imageUrl, setImageUrl] = useState(
+    "https://images.unsplash.com/photo-1500964757637-c85e8a162699?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHNjZW5lcnl8ZW58MHx8MHx8fDA="
+  );
+  const socket = io("https://sling-backend.onrender.com");
 
-    useEffect(() => {
-      // Fetch the image from your Node.js server
-      axios
-        .get(
-          `https://sling-backend-masumpeacock-gmailcom.vercel.app/api/get-image`
-        )
-        .then((response) => {
-          console.log("response", response?.data?.data?.image);
-          setImageUrl(
-            `data:image/png;base64,${response?.data?.data?.image}`
-          );
-        })
-        .catch((error) => {
-          console.error("Error fetching image:", error);
-        });
-    }, []);
+  socket.on("imageUploaded", (data) => {
+    setImageUrl(`data:image/png;base64,${data.data.image}`)
+  })
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://sling-backend.onrender.com/api/get-image`
+      )
+      .then((response) => {
+        console.log("response", response?.data?.data?.image);
+        setImageUrl(
+          `data:image/png;base64,${response?.data?.data?.image}`
+        );
+      })
+      .catch((error) => {
+        console.error("Error fetching image:", error);
+      });
+  }, []);
+
   return <div>
     <p>Andalib</p>
     {imageUrl && <img src={imageUrl} alt="Captured" />}
-    </div>;
+  </div>;
 }
